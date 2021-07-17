84df05fba3661df2841f41e079605a3e2354bad4
matheus-miranda <matheus.sm86@gmail.com>
2021-07-17 00:03:21 -0300

    Add viewBinding to MainActivity.kt

--- app/src/main/java/br/com/mmdevelopment/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/mmdevelopment/businesscard/ui/MainActivity.kt
@@ -6,11 +6,10 @@ import br.com.mmdevelopment.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
