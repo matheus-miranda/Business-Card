e06ecacca2fa24e1f4da44779c57cf13e7d12ed1
matheus-miranda <matheus.sm86@gmail.com>
2021-07-18 17:28:51 -0300

    String value returned from TextInputLayout

--- app/src/main/java/br/com/mmdevelopment/businesscard/App.kt
+++ app/src/main/java/br/com/mmdevelopment/businesscard/App.kt
@@ -1,9 +1,17 @@
 package br.com.mmdevelopment.businesscard
 
 import android.app.Application
+import com.google.android.material.textfield.TextInputLayout
 
 /**
  * This class will run when our app starts
  */
 class App : Application() {
+
+    // Return String value for TextInput and set text to received value
+    var TextInputLayout.text: String
+        get() = editText?.text.toString()
+        set(value) {
+            editText?.setText(value)
+        }
 }
