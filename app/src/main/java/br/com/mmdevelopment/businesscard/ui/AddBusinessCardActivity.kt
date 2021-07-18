3fb3fe127968d627072bfea1e45d27a03b80e4a6
matheus-miranda <matheus.sm86@gmail.com>
2021-07-18 16:37:28 -0300

    Create AddBusinessCard layout

---
+++ app/src/main/java/br/com/mmdevelopment/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,12 @@
+package br.com.mmdevelopment.businesscard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.mmdevelopment.businesscard.R
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
