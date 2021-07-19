b343d2fd653f16946869a8335f84cb9b9e8438ce
matheus-miranda <matheus.sm86@gmail.com>
2021-07-19 15:21:09 -0300

    Instantiate database and repository when the app starts

--- app/src/main/java/br/com/mmdevelopment/businesscard/App.kt
+++ app/src/main/java/br/com/mmdevelopment/businesscard/App.kt
@@ -1,9 +1,13 @@
 package br.com.mmdevelopment.businesscard
 
 import android.app.Application
+import br.com.mmdevelopment.businesscard.data.AppDatabase
+import br.com.mmdevelopment.businesscard.data.BusinessCardRepository
 
 /**
  * This class will run when our app starts
  */
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessCardDao()) }
 }
